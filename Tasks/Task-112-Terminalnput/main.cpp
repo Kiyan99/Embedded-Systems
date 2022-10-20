004e0812d579555aea44c401e75c0ef92fce669b
Ghost-web2 <kiyan.ahmadi@students.plymouth.ac.uk>
2022-10-20 13:05:54 +0100

    112

--- Tasks/Task-112-Terminalnput/main.cpp
+++ Tasks/Task-112-Terminalnput/main.cpp
@@ -23,6 +23,11 @@ int main()
     printf("You entered character %c which has the ASCII code %d\n", c2, c2);
 
     //scanf
+    printf("\n\nType in an integer for ON time(in ms)");
+    int on_ms;
+    int on_time = scanf("%d", &on_ms);
+    printf("You entered %d correct integer values. The value was %dms\n", on_time, on_ms);
+
     printf("\n\nTo set the delay (in ms), type in an integer number and press return\n");
     int delay_ms;
     int parsed = scanf("%d", &delay_ms);
@@ -30,11 +35,11 @@ int main()
     printf("You entered %d correct integer values. The value was %dms\n", parsed, delay_ms);
 
     // Stop
-    while (parsed > 0) {
+    while ((parsed > 0)&&(on_time > 0)) {
         wait_us(delay_ms * 1000);
-        led1 = 1;
-        led2 = 1;
-        led3 = 1;
+        led1 = on_ms;
+        led2 = on_ms;
+        led3 = on_ms;
         wait_us(delay_ms * 1000);
         led1 = 0;
         led2 = 0;
